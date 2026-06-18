97be73aec6b87d52a87b6d325c335b73b00ffd2a
Suyesha11 <suyeshapatil217@gmail.com>
2026-06-18 18:56:24 +0200

    Added productPage tests

--- pages/ProductPage.ts
+++ pages/ProductPage.ts
@@ -1,19 +1,33 @@
-import { test , expect } from "../fixture/auth-fixture";
+
 import { Locator, Page } from "@playwright/test";
 
 export class ProductPage{
     private readonly page:Page;
-    readonly productList:Locator;
+    private readonly productList:Locator;
     private readonly productFilterDropdown:Locator;
+    private readonly productName:Locator;
+    private readonly productPrices:Locator;
+    private readonly firstProductCard :Locator;
+    private readonly shoppingCartBadge:Locator;
+    private readonly shoppingCartLink:Locator;
+    private readonly lastProductCard:Locator;
+    private readonly productImages:Locator;
 
     constructor (page:Page){
         this.page = page;
         this.productList = page.getByTestId("inventory-list");
-        this.productFilterDropdown = page.getByTestId("product_sort_container");
+        this.productFilterDropdown = page.getByTestId("product-sort-container");
+        this.productName= this.productList.getByTestId("inventory-item-name");
+        this.productPrices= page.getByTestId("inventory-item-price");
+        this.firstProductCard= page.getByTestId("inventory-item").first();
+        this.shoppingCartBadge=page.getByTestId("shopping-cart-badge");
+        this.lastProductCard= page.getByTestId("inventory-item").last();
+        this.shoppingCartLink= page.getByTestId("shopping-cart-link");
+        this.productImages= page.locator("img.inventory_item_img");
     }
 
-    async getProductCount():Promise<number>{
-        return await this.productList.getByTestId("inventory-item").count();
+    async getProductNames():Promise<string[]>{
+        return await this.productName.allTextContents();
     }
 
     async getProductFilters():Promise<number>{
@@ -23,5 +37,60 @@ export class ProductPage{
     async selectProductFilter(filterName:string):Promise<void>{
         await this.productFilterDropdown.selectOption({label:filterName});
     }
+
+    async getProductPrice():Promise<number[]>{
+        const prices = await this.productPrices.allTextContents();
+        return prices.map(price=>parseFloat(price.replace("$","")));
+    }
+
+    async getFirstProductName(): Promise<string>{
+        const name = await this.firstProductCard.getByTestId("inventory-item-name").textContent();
+        return name?.trim() ?? "";
+    }
+
+    async clickFirstProductName(){
+        await this.firstProductCard.getByTestId("inventory-item-name").click();
+    }
+
+     getFirstProductRemoveButton(){
+        return this.firstProductCard.getByRole('button', {name: "Remove"});
+    }
+
+    async addFirstProductToCart(){
+        await this.firstProductCard.getByRole('button', {name: "Add to cart"}).click();
+    }
+
+    async addLastProductToCart(){
+        await this.lastProductCard.getByRole('button', {name: "Add to cart"}).click();
+    }
+
+     getShoppingCartBadgeLocator():Locator{
+        return this.shoppingCartBadge;
+    }
+
+    async getShoppingCartBadgeText(){
+        return await this.shoppingCartBadge.textContent();
+    }
+
+    async removeFirstProductFromCart(){
+        await this.getFirstProductRemoveButton().click();
+    }
+
+     getProductList(){
+        return this.productList;
+    }
+
+    getShoppingCartLink():Locator{
+        return this.shoppingCartLink;
+    }
+
+    async getProductImageSrcs(){
+        return await this.productImages.evaluateAll(
+            images => images.map(img => img.getAttribute("src") ?? "")
+        );
+
+    }
+
+
 
 }
--- tests/ProductPage.spec.ts
+++ tests/ProductPage.spec.ts
@@ -1,33 +1,90 @@
 import { ProductPage } from "../pages/ProductPage";
-import {test , expect} from "../fixture/auth-fixture";
+import { test, expect } from "../fixture/auth-fixture";
 import { productFilters } from "../utils/productFilters";
 
-test.describe("Product Page Tests",()=>{
-    let productPage:ProductPage;
-    test.beforeEach(async({loginAsStandardUser})=>{
-        productPage = new ProductPage(loginAsStandardUser);
-    });
+test.describe("Product Page Tests for Standard User", () => {
+  let productPage: ProductPage;
+  test.beforeEach(async ({ loginAsStandardUser }) => {
+    productPage = new ProductPage(loginAsStandardUser);
+  });
 
-    test("should display correct number of products",async()=>{
-       const productCount = await productPage.getProductCount();
-        expect(productCount).toBe(6);
-    },
-);
+  test("should display correct number of product filters", async () => {
+    const filterCount = await productPage.getProductFilters();
+    expect(filterCount).toBe(4);
+  });
 
-    test("should display correct number of product filters",async()=>{
-        const filterCount = await productPage.getProductFilters();
-        expect(filterCount).toBe(4);
-    });
+  test("should sort products by ascending name", async () => {
+    await productPage.selectProductFilter(productFilters.ascendingName);
+    const productNames = await productPage.getProductNames();
+    const sortedNames = [...productNames].sort();
+    expect(productNames).toEqual(sortedNames);
+  });
 
-    test("should sort products by ascending name",async()=>{
-        await productPage.selectProductFilter(productFilters.ascendingName);
-        const firstProductName = await productPage.productList.getByTestId("inventory-item").first().getByTestId("inventory-item-name").textContent();
-        expect(firstProductName).toBe("Sauce Labs Backpack");
-    });
+  test("should sort products by descending name", async () => {
+    await productPage.selectProductFilter(productFilters.descendingName);
+    const productNames = await productPage.getProductNames();
+    const sortedNames = [...productNames].sort().reverse();
+    expect(productNames).toEqual(sortedNames);
+  });
 
-    test("should sort products by descending name",async()=>{
-        await productPage.selectProductFilter(productFilters.descendingName);
-        const firstProductName = await productPage.productList.getByTestId("inventory-item").first().getByTestId("inventory-item-name").textContent();
-        expect(firstProductName).toContain("Shirt");
+  test("should sort products by ascending price", async () => {
+    await productPage.selectProductFilter(productFilters.ascendingPrice);
+    const prices = await productPage.getProductPrice();
+    for (let i = 0; i < prices.length - 1; i++) {
+      expect(prices[i]).toBeLessThanOrEqual(prices[i + 1]);
+    }
+  });
 
-})
+  test("should sort products by descending price", async () => {
+    await productPage.selectProductFilter(productFilters.descendingPrice);
+    const prices = await productPage.getProductPrice();
+    for (let i = 0; i < prices.length - 1; i++) {
+      expect(prices[i]).toBeGreaterThanOrEqual(prices[i + 1]);
+    }
+  });
+
+  test("should be able to add and remove product from cart", async () => {
+    await productPage.addFirstProductToCart();
+    expect(await productPage.getShoppingCartBadgeText()).toBe("1");
+
+    await productPage.removeFirstProductFromCart();
+    await expect(productPage.getShoppingCartBadgeLocator()).not.toBeVisible();
+  });
+
+  test("adding product to cart should change the text of add to cart button to remove", async () => {
+    await productPage.addFirstProductToCart();
+    await expect(productPage.getFirstProductRemoveButton()).toBeVisible();
+  });
+
+  test("should naviagte to product details page when clicking on product name", async ({
+    page,
+  }) => {
+    const productName = await productPage.getFirstProductName();
+    await productPage.clickFirstProductName();
+    await expect(page).toHaveURL(/inventory-item.html\?id=\d+/);
+    await expect(page.getByTestId("inventory-item-name")).toHaveText(
+      productName,
+    );
+  });
+
+  test("should be able to add multiple products to cart and verify badge count", async () => {
+    await productPage.selectProductFilter(productFilters.ascendingPrice);
+    await productPage.addFirstProductToCart();
+    await productPage.addLastProductToCart();
+    expect(await productPage.getShoppingCartBadgeText()).toBe("2");
+  });
+});
+
+test.describe("Product page tests for Problem User", () => {
+  let productPage: ProductPage;
+
+  test.beforeEach(async ({ loginAsProblemUser }) => {
+    productPage = new ProductPage(loginAsProblemUser);
+  });
+
+  test("each product should have a unique image", async () => {
+    const imgSrcs = await productPage.getProductImageSrcs();
+    const allSame = imgSrcs.every((src) => src === imgSrcs[0]);
+    expect(allSame).toBe(true);
+  });
+});
